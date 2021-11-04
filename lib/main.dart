b30687b54fc98602876bb54f7fb4dd78b6e1f64e
Ream Sadaoui <sdi.ream@gmail.com>
2021-11-04 19:23:16 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:chomeurs/Vert/page_resultat_vert.dart';
 import 'package:chomeurs/Orange/page_resultat_orange.dart';
 import 'package:chomeurs/Rouge/page_rouge.dart';
+import 'package:chomeurs/Parametres/parametres.dart';
 
 void main() => runApp(MyApp());
 //TESSST
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF9EA2A6),
       ),
       // ignore: prefer_const_constructors
-      home: StatistiqueScreen(),
+      home: Parametres(),
     );
   }
 }
