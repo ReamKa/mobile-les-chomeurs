3320f69c099554c141a1758fa0e22875270f45f0
Ream Sadaoui <sdi.ream@gmail.com>
2021-11-04 21:59:55 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:chomeurs/Vert/page_resultat_vert.dart';
 import 'package:chomeurs/Orange/page_resultat_orange.dart';
 import 'package:chomeurs/Rouge/page_rouge.dart';
 import 'package:chomeurs/Parametres/parametres.dart';
+import 'package:chomeurs/Profile/page_profile.dart';
 
 void main() => runApp(MyApp());
 //TESSST
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF9EA2A6),
       ),
       // ignore: prefer_const_constructors
-      home: Parametres(),
+      home: PageParametres(),
     );
   }
 }
